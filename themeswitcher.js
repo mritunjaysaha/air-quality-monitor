e3510887fe64c1c2b22acdb11081b39f95701717
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-04-30 17:47:04 +0530

    theme-switch does not becomes invisible when clicked

--- themeswitcher.js
+++ themeswitcher.js
@@ -17,13 +17,19 @@ function clickHandler() {
     //         setTheme();
     //     }
     // }
+    const themeSwitchRound = document.getElementById("theme-switch-round");
     if (this.checked) {
         localStorage.setItem("theme", "dark");
         document.body.className = "dark";
+
+        themeSwitchRound.classList.add("local-dark");
+        themeSwitchRound.classList.remove("local-light");
         console.log("dark");
     } else {
         localStorage.setItem("theme", "light");
         document.body.className = "light";
+        themeSwitchRound.classList.add("local-light");
+        themeSwitchRound.classList.remove("local-dark");
         console.log("light");
     }
 }
